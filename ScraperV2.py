f874902e6e55fb85ca4bfe65a2b9a77b72cc3b48
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-05-16 20:07:17 -0400

    Forked V1 of Scraper
    forked V1 of scraper to V2. Major change includes: condensing parse_url() and cycle_links() functions into one function named parse_url()

--- ScraperV2.py
+++ ScraperV2.py
@@ -1,4 +1,4 @@
-#for any given webpage, the user has the option of finding all URL/Hyperlinks // the top 3 words that appear on webpage // eventually map out the webserver and all its links
+#for any given webpage, the user has the option of finding all URL // the top 3 words that appear on webpage // eventually map out the webserver and all its links
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import re
@@ -6,19 +6,18 @@ import re
 
 #takes HTML, parses through and returns all anchor tags
 def parse_url(url):
+    all_urls = []
     html = urllib.request.urlopen(url).read() #opens URL
     sifter = BeautifulSoup(html, 'html.parser') #sifts and return one large string
-    tags = sifter('a', None) #extracts all anchor tags
-    return tags
-
-#takes all anchor tags, captures URLs and returns total list of URL
-def cycle_links(anchor_list):
-    all_urls = []
+    anchor_list = sifter('a', None) #extracts all anchor tags
     for tag in anchor_list:
         #gets HTML tag as string
         x = tag.get('href', None)
         all_urls.append(x)
+
     return all_urls
+
+#takes all anchor tags, captures URLs and returns total list of URL
 
 #passes a keyword
 def keyword_parser(keyword):
